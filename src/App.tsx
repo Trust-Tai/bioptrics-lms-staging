d3de9c61dff5d9a878abc3a5714cc9fe27cb54ca
amit7046 <amitdudhat.ad1@gmail.com>
2025-11-11 21:52:15 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="welcome-container">
+        <h1>Welcome to Bioptrics LMS</h1>
+        <p>Learning Management System</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
